6b65318b3dd25201b3fdd1c77cda9f4f52ab72cf
davidcoats <david@getsafetycone.com>
2023-05-16 09:54:13 -0700

    Initial commit.

---
+++ source/R5T.T0197/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0197
+{
+	/// <summary>
+	/// Library descriptor types library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.T0197/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.T0197
+Library descriptor types library.
