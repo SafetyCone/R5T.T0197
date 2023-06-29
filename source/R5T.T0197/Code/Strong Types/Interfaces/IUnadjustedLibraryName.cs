b60b79f45fc91a2fe4ff2ed90ea1130c5f9ff896
davidcoats <david@getsafetycone.com>
2023-06-29 01:24:53 -0700

    Interim changes.

--- source/R5T.T0197/Code/Strong Types/Interfaces/IUnadjustedLibraryName.cs
+++ source/R5T.T0197/Code/Strong Types/Interfaces/IUnadjustedLibraryName.cs
@@ -7,7 +7,9 @@ using R5T.T0179;
 namespace R5T.T0197
 {
     /// <summary>
-    /// Strongly-types a string as a %%%.
+    /// Strongly-types a string as a unadjusted library name.
+    /// Library names might be adjusted for many reasons. (For example, is the GitHub repository for the library private?)
+    /// This is the primary library name type.
     /// </summary>
     [StrongTypeMarker]
     public interface IUnadjustedLibraryName : IStrongTypeMarker,
